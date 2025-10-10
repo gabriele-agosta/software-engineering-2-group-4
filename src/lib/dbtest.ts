ce933fb5d69475d4ed3e11153f22483c11f6e44a
Andrea Maria Spina s334214 <s334214@studenti.polito.it>
2025-10-10 15:12:48 +0200

    definition of the schemas and prisma initialization #1

---
+++ src/lib/dbtest.ts
@@ -0,0 +1,20 @@
+import type { Ticket, Service, Prisma } from "@prisma/client";
+
+const service: Service = {
+  //if everithing works, you should be able to see the Service type structure
+  // overing the type definition on the previous row
+  name: "service",
+  id: 1n,
+  expected_waiting_time: "10 minutes"
+};
+
+const ticket: Ticket = {
+  //if everithing works, you should be able to see the Ticket type structure
+  // overing the type definition on the previous row
+  id: 1n,
+  service_id: 1n,
+  taken_at: new Date(),
+  estimated_waiting_time: "10 minutes",
+  waiting_time: null,
+  served: false,
+};
