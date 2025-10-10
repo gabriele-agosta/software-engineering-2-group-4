6b48b908347a639d8b8838c1aa7b42601c5fd129
SERGIO MAESTRALE s346253 <s346253@studenti.polito.it>
2025-10-10 20:47:15 +0200

    feat: implement createTicketAction and createTicket function for ticket management #7

--- src/app/lib/actions/tickets.ts
+++ src/app/lib/actions/tickets.ts
@@ -1,3 +1,15 @@
 'use server';
 
 import { revalidatePath } from 'next/cache';
+import { createTicket } from '../services/tickets.service';
+
+//fast implementation just for have a function fro the frontend to call when desisgning the UI
+export async function createTicketAction(serviceId: number) {
+    try {
+        const ticket = await createTicket(serviceId);
+        revalidatePath('/tickets');
+        return ticket;
+    } catch (error) {
+        throw error;
+    }
+}
--- src/app/lib/services/tickets.service.ts
+++ src/app/lib/services/tickets.service.ts
@@ -1,1 +1,15 @@
 import { CreateTicketRequest, Ticket } from "../types/ticket";
+
+export async function createTicket(serviceId: number): Promise<Ticket> {
+    try {
+        if (!serviceId) {
+            throw new Error("Service is required to create a ticket.");
+        }
+        // TODO Implement repository call after database is set up
+        return new Ticket(1, serviceId);
+    } 
+    catch (error) {
+        throw error;
+    }
+
+}
